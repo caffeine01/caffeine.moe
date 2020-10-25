9de6b35477300b8d882b3f2595ce400e6ff44944
Juicyexe <juicyexe420@gmail.com>
2020-10-25 12:37:36 +0000

    up

--- particles.js
+++ particles.js
@@ -33,7 +33,7 @@ var pJS = function(tag_id, params){
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#FFF'
+          color: '#171a1f'
         },
         polygon: {
           nb_sides: 5
